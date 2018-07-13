5cb2bea9c5f691544040114dd4234ca616922d03
Michael Lacey <laceymwes@gmail.com>
2018-07-13 10:42:35 -0600

    - Create custom ArrayAdapter class for GridView.

--- app/src/main/java/edu/cnm/deepdive/tileslide/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/tileslide/controller/MainActivity.java
@@ -2,14 +2,43 @@ package edu.cnm.deepdive.tileslide.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
 import edu.cnm.deepdive.tileslide.R;
+import edu.cnm.deepdive.tileslide.model.Frame;
+import edu.cnm.deepdive.tileslide.view.FrameAdapter;
+import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+  private static int PUZZLE_SIZE = 3;
+
+  private Frame frame;
+  private FrameAdapter adapter;
+  private GridView tileGrid;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    tileGrid = findViewById(R.id.tile_grid);
+    tileGrid.setNumColumns(PUZZLE_SIZE);
+    tileGrid.setOnItemClickListener(this);
+    createPuzzle();
   }
 
+
+  @Override
+  public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+    // TODO: Implement tile sliding by responding to clicks, and invoking any relevant
+    // methods in the Frame class.
+  }
+
+  private void createPuzzle() {
+    frame = new Frame(PUZZLE_SIZE, new Random());
+    adapter = new FrameAdapter(this, frame);
+    tileGrid.setAdapter(adapter);
+  }
 }
---
+++ app/src/main/java/edu/cnm/deepdive/tileslide/view/FrameAdapter.java
@@ -0,0 +1,85 @@
+package edu.cnm.deepdive.tileslide.view;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import edu.cnm.deepdive.tileslide.R;
+import edu.cnm.deepdive.tileslide.model.Frame;
+import edu.cnm.deepdive.tileslide.model.Tile;
+
+public class FrameAdapter extends ArrayAdapter<Tile> {
+
+  private int size;
+  private Bitmap[] tileImages; // tiles from image
+  private Bitmap noTileImage; // empty space
+  private Frame frame;
+  private Tile[] tiles;
+
+  public FrameAdapter(@NonNull Context context, @NonNull Frame frame) {
+    super(context, R.layout.tile_item);
+    this.frame = frame;
+    size = frame.getTiles().length;
+    tiles = new Tile[size * size];
+    copyModelTiles();
+    addAll(tiles);
+    sliceBitmap();
+  }
+
+  @NonNull
+  @Override
+  public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    if (convertView == null) {
+      convertView = LayoutInflater.from(getContext()).inflate(R.layout.tile_item, null, false);
+    }
+    Tile tile = getItem(position);
+    TileView tileView = convertView.findViewById(R.id.tile_image);
+    if (tile != null) {
+      tileView.setImageBitmap(tileImages[tile.getNumber()]);
+    } else {
+      tileView.setImageBitmap(noTileImage);
+    }
+    return convertView;
+  }
+
+  @Override
+  public void notifyDataSetChanged() {
+    copyModelTiles();
+    setNotifyOnChange(false);
+    // TODO Possible modify this if we want to add animation.
+    clear();
+    addAll(tiles);
+    super.notifyDataSetChanged();
+  }
+
+  private void copyModelTiles() {
+    Tile[][] source = frame.getTiles();
+    for (int row = 0; row < size; row++) {
+      System.arraycopy(source[row], 0, tiles, row * size, size);
+
+    }
+  }
+
+  private void sliceBitmap() {
+    Drawable drawable = ContextCompat.getDrawable(getContext(), R.drawable.android_robot_circle);
+    Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+    tileImages = new Bitmap[size * size];
+    int imageWidth = bitmap.getWidth();
+    int imageHeight = bitmap.getHeight();
+    for (int i = 0; i < tileImages.length; i++) {
+      int row = i / size;
+      int col = i % size;
+      tileImages[i] = Bitmap.createBitmap(bitmap, col * imageWidth / size, row * imageHeight / size,
+          imageWidth / size, imageHeight / size);
+    }
+    noTileImage = Bitmap.createBitmap(tileImages[tileImages.length - 1]);
+    noTileImage.eraseColor(ContextCompat.getColor(getContext(), R.color.puzzleBackground));
+  }
+}
